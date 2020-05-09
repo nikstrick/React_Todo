a23522513b020cc92e161541deb5e2fc99668ec5
Nikhil Ranjan <nikhilranjan227@gmail.com>
2020-05-09 01:01:43 +0100

    Updates

---
+++ precache-manifest.ae7d46f8e4fba88c7c6482547dc11141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87fa18e228deb2f39afee31394bcb8b",
+    "url": "/React_Todo/index.html"
+  },
+  {
+    "revision": "98084daa1cea44567621",
+    "url": "/React_Todo/static/css/main.72b64b99.chunk.css"
+  },
+  {
+    "revision": "8ddf10c699cd8aacf104",
+    "url": "/React_Todo/static/js/2.798ccca1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Todo/static/js/2.798ccca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98084daa1cea44567621",
+    "url": "/React_Todo/static/js/main.58698da0.chunk.js"
+  },
+  {
+    "revision": "52b48688c1ecdb742745",
+    "url": "/React_Todo/static/js/runtime-main.141cd5d0.js"
+  }
+]);
